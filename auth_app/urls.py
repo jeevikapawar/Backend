6f5f616e5b766b0d90d8d4d86180f1b8f3bfd67f
Jeevika Pawar <75422053+jeevikapawar@users.noreply.github.com>
2024-04-11 00:40:38 -0700

    Add files via upload

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -6,10 +6,10 @@ from.views import register_view
 urlpatterns = [
 # path('',views.register_view,name='register'),
     path('',views.login_view,name='login'),
-    path('student_register/', views.register_student, name='student_register'),  # URL for student registration
-    path('teacher-register/', views.register_view, name='teacher_register'),
+    path('register/', views.register_student, name='student_register'),  # URL for student registration
+    path('register/', views.register_view, name='teacher_register'),
     path('logout/',views.logout_view,name='logout'),
     path('student_dashboard/',views.student_dashboard, name='student_dashboard'),
-   # path('teacher_dashboard/',views.dashboard_view,name='teacher-dashboard'),
+    path('teacher_dashboard/',views.teacher_dashboard,name='teacher-dashboard'),
     #path('display-student-dashboard/',views.student_view ,name='student-dashboard')
 ]
